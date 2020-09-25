af423af477f861f0fdc3fa147742affd1a4ae0d9
Syond <syond94@gmail.com>
2020-09-25 09:28:18 -0300

    new features

--- whiteboarding-tests/index.js
+++ whiteboarding-tests/index.js
@@ -30,11 +30,111 @@ function iniciaisEmMaisculo() {
   for (x = 0; x < strSplited.length; x++) {
     //Encontra as posições que estão vazias
     if (strSplited[x] == " ") {
-        //Pega a próxima posição, transforma em maisculo e substitui no Array
+      //Pega a próxima posição, transforma em maisculo e substitui no Array
       strSplited[x + 1] = strSplited[x + 1].toUpperCase();
     }
   }
-  
+
   //Converte o Array para String
   return alert(strSplited.join(""));
+}
+
+function adicionarNoInicioDoArray() {
+  const cestaDeFrutas = ["banana", "abacaxi", "uva", "melancia"];
+
+  const novaFruta = prompt(
+    "Adicione algo ao inicio da cesta de frutas :)" +
+      "\n" +
+      cestaDeFrutas +
+      " ... o que mais ?"
+  );
+
+  const novaCestaDeFrutas = [novaFruta, ...cestaDeFrutas];
+
+  return alert("Essa é sua nova cesta de frutas: " + novaCestaDeFrutas);
+}
+
+function adicionarNoFimDoArray() {
+  const cestaDeFrutas = ["banana", "abacaxi", "uva", "melancia"];
+
+  const novaFruta = prompt(
+    "Adicione algo ao final da cesta de frutas :)" +
+      "\n" +
+      cestaDeFrutas +
+      " ... o que mais ?"
+  );
+
+  const novaCestaDeFrutas = [...cestaDeFrutas, novaFruta];
+
+  return alert("Essa é sua nova cesta de frutas: " + novaCestaDeFrutas);
+}
+
+function adicionarNoInicioDoArraySemSpreadCriandoNovoArray() {
+  const cestaDeFrutas = ["banana", "abacaxi", "uva", "melancia"];
+
+  const novaFruta = prompt(
+    "Adicione algo ao inicio da cesta de frutas :)" +
+      "\n" +
+      cestaDeFrutas +
+      " ... o que mais ?"
+  );
+
+  let novaCestaDeFrutas = [];
+
+  //Adiciona a nova fruta na primeira posição do Array
+  novaCestaDeFrutas[0] = novaFruta;
+
+  //Varre todo o Array cestaDeFrutas e copia os valores para novaCestaDeFrutas a partir da posição 1
+  for (x = 0; x < cestaDeFrutas.length; x++) {
+    novaCestaDeFrutas[x + 1] = cestaDeFrutas[x];
+  }
+
+  return alert("Essa é sua nova cesta de frutas: " + novaCestaDeFrutas);
+}
+
+function adicionarNoFimDoArraySemSpreadCriandoNovoArray() {
+  const cestaDeFrutas = ["banana", "abacaxi", "uva", "melancia"];
+
+  const novaFruta = prompt(
+    "Adicione algo ao inicio da cesta de frutas :)" +
+      "\n" +
+      cestaDeFrutas +
+      " ... o que mais ?"
+  );
+
+  let novaCestaDeFrutas = [];
+
+  //Varre todo o Array cestaDeFrutas e copia os valores para novaCestaDeFrutas
+  for (x = 0; x < cestaDeFrutas.length; x++) {
+    novaCestaDeFrutas[x] = cestaDeFrutas[x];
+  }
+
+  //Adiciona a nova fruta na última posição do Array com a propriedade "length", que começa em 1.
+  novaCestaDeFrutas[novaCestaDeFrutas.length] = novaFruta;
+
+  return alert("Essa é sua nova cesta de frutas: " + novaCestaDeFrutas);
+}
+
+function inverterArray() {
+  let arrayOriginal = [11, 12, 13, 14, 15, 16, 17];
+
+  alert("Temos esse Array aqui: " + arrayOriginal);
+
+  return alert(
+    "E esse é o mesmo array só que invertido: " + arrayOriginal.reverse()
+  );
+}
+
+function inverterArrayUtilizandoForCriandoNovoArray() {
+  let arrayOriginal = [11, 12, 13, 14, 15, 16, 17];
+
+  alert("Temos esse Array aqui: " + arrayOriginal);
+
+  let arrayInvertido = [];
+
+  for (x = arrayOriginal.length - 1; x > -1; x--) {
+    arrayInvertido.push(arrayOriginal[x]);
+  }
+
+  return alert("E esse é o mesmo array só que invertido: " + arrayInvertido);
 }
